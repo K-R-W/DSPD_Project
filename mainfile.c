35a2263c10f81b1e98ac6d72d981580217cead72
appmonster007 <prathitlenovoa1000@gmail.com>
2019-10-27 12:07:09 +0530

    made array for easy access

--- mainfile.c
+++ mainfile.c
@@ -2,21 +2,16 @@
 #include "cricket_structure.h"
 void main()
 {
-    struct player player1 = {"jon","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","out"};
-    struct player player2 = {"snow","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","out"};
-    struct player player3 = {"tyrion","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","out"};
-    struct player player4 = {"olenna","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"};
-    struct player player5 = {"geoffrey","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"};
-    struct player player6 = {"tywin","M2019.1",56,30,2,1,310,4,1,45,1,"all-rounder","not out"};
-    struct player player7 = {"illiana","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"};
-    struct player player8 = {"mance","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"};
-    struct player player9 = {"george","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"};
-    struct player player10 = {"night king","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"};
-    struct player player11 = {"ollie","M2019.1",56,30,2,1,310,4,1,45,1,"all-rounder","not out"};
-    struct player player12 = {"samwell","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"};
-    struct player player13 = {"drogon","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"};
-    struct player player14 = {"mellisandre","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"};
-    struct player player15 = {"agatha","M2019.1",56,30,2,1,310,4,1,45,1,"all-rounder","not out"};
+    struct player player1[15] = {{"jon","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","out"}, {"snow","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","out"}, {"tyrion","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","out"}, {"olenna","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"}, {"geoffrey","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"}, {"tywin","M2019.1",56,30,2,1,310,4,1,45,1,"all-rounder","not out"};
+    , {"illiana","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"}
+    , {"mance","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"}
+    , {"george","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"}
+    , {"night king","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"}
+    , {"ollie","M2019.1",56,30,2,1,310,4,1,45,1,"all-rounder","not out"}
+    , {"samwell","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"}
+    , {"drogon","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"}
+    , {"mellisandre","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"}
+    , {"agatha","M2019.1",56,30,2,1,310,4,1,45,1,"all-rounder","not out"}};
     struct team team1[15] = {player1,player2,player3,player4,player5,player6,player7,player8,player9,player10,player11,player12,player13,player14,player15};
 
 }
