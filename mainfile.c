f37e3c709aa3b2b62569a6bee97b448b51ccafde
appmonster007 <prathitlenovoa1000@gmail.com>
2019-10-27 12:26:58 +0530

    new team

--- mainfile.c
+++ mainfile.c
@@ -19,4 +19,21 @@ void main()
     , {"agatha","M2019.1",56,30,2,1,310,4,1,45,1,"all-rounder","not out"}};
     struct team team1[15] = {player1[0],player1[1],player1[2],player1[3],player1[4],player1[5],player1[6],player1[7],player1[8],player1[9],player1[10],player1[11],player1[12],player1[13],player1[14]};
 
+
+    struct player player2[15] = {{"sumit","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","out"}
+    , {"manohar","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","out"}
+    , {"pushkar","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","out"}
+    , {"sachin","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"}
+    , {"makarand","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"}
+    , {"bharat","M2019.1",56,30,2,1,310,4,1,45,1,"all-rounder","not out"};
+    , {"kushal","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"}
+    , {"sagar","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"}
+    , {"bhau","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"}
+    , {"ashok","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"}
+    , {"bharat","M2019.1",56,30,2,1,310,4,1,45,1,"all-rounder","not out"}
+    , {"ganesh","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"}
+    , {"nisarg","M2019.1",56,30,2,1,310,4,1,45,1,"bowler","not out"}
+    , {"jayesh","M2019.1",56,30,2,1,310,4,1,45,1,"batsman","not out"}
+    , {"mahesh","M2019.1",56,30,2,1,310,4,1,45,1,"all-rounder","not out"}};
+    struct team team2[15] = {player2[0],player2[1],player2[2],player2[3],player2[4],player2[5],player2[6],player2[7],player2[8],player2[9],player2[10],player2[11],player2[12],player2[13],player2[14]};
 }
