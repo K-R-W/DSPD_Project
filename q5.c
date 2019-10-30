fa5c60decc4cd98cf049cb8e4057b8739dd6d66c
KaustubhWankhede <kaustubhwankhede@gmail.com>
2019-10-30 10:57:49 +0530

    q5 has been added

---
+++ q5.c
@@ -0,0 +1,15 @@
+int highest_avg(struct team teams[][]){
+    int i,j,max=0;
+    char *maxname;
+    for(i=0;i<2;i++){
+        for(j=0;j<15;j++){
+            if(teams[i].all_players[j].previous_avg>max){
+                max=teams[i].all_players[j].previous_avg;
+                maxname=teams[i].all_players[j].player_id;
+            }    
+        }
+    }
+    printf("player with highest avg is %s with avg %d",maxname, max);
+    
+
+}
